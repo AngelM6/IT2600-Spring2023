ea3fee0803820128892cb5cf9b4cc2e68fdef960
AngelM6 <60329638+AngelM6@users.noreply.github.com>
2023-03-16 01:03:36 -0400

    Add files via upload

---
+++ Lab3/courses.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>Course Id</th><th>Title</th><th>Prerquisites</th><th>Description</th><th>Credit Hours</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "localhost";
+$username = "Angel";
+$password = "password";
+$dbname = "it1150";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT course_id, title, prerequisites, description, credit_hrs FROM courses");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+
+</body>
+</html>
